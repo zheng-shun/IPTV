45982147ae097d3b4e9189ae1b3d0c85ce686dd8
xiaoxiaozhou-zcx <109648382+xiaoxiaozhou-zcx@users.noreply.github.com>
2024-05-18 18:52:41 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,27 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "广东珠江",
+    "广东影视",
+    "岭南戏曲",
+    "大湾区卫视",
+]
+open_online_search = True
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 15
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "all"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = False
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+]
+open_multicast = True
+region_list = ["all"]
